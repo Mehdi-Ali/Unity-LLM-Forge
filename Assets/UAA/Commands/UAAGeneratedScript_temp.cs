a0f67b6b2374eb1bd4574082106152c34f30c27d
Mehdi-Ali <alimehdi.offi@gmail.com>
2024-01-14 20:57:14 -0500

    IDK

--- Assets/UAA/Commands/UAAGeneratedScript_temp.cs
+++ Assets/UAA/Commands/UAAGeneratedScript_temp.cs
@@ -6,11 +6,12 @@ public class UAAGeneratedScript : EditorWindow
     [MenuItem("Edit/Do Task")]
     private static void DoTask()
     {
-        Vector3 position;
         for (int i = 0; i < 10; i++)
         {
-            position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f));
-            GameObject sphere = UnityEngine.Object.Instantiate(UnityEngine.GameObject.CreatePrimitive(PrimitiveType.Sphere), position, Quaternion.identity);
+            Vector3 randomPosition = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            GameObject sphere = UnityEngine.GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            UnityEngine.Object.Instantiate(sphere, randomPosition, Quaternion.identity);
+            UnityEngine.Object.DestroyImmediate(sphere);
         }
     }
 }

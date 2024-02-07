93c90ec97e3b3b8995b162ffa79caed4f93cb2cc
Mehdi-Ali <alimehdi.offi@gmail.com>
2024-02-07 02:19:41 -0500

    playing around

--- Assets/UAA/Commands/UAAGeneratedScript_temp.cs
+++ Assets/UAA/Commands/UAAGeneratedScript_temp.cs
@@ -6,22 +6,9 @@ public class UAAGeneratedScript_temp : EditorWindow
     [MenuItem("Edit/UAA - Unity AI Assistant/Execute")]
     private static void DoTask()
     {
-        // Create a room using walls and wood planks
-        var wallPath = "Assets/Resources_moved/Prefabs/Wall_04.prefab";
-        var woodPlankPath = "Assets/Resources_moved/Prefabs/WoodPlank_01.prefab";
-        
-        // Load the prefabs from the paths
-        UnityEngine.Object wallPrefab = AssetDatabase.LoadAssetAtPath(wallPath, typeof(GameObject));
-        UnityEngine.Object woodPlankPrefab = AssetDatabase.LoadAssetAtPath(woodPlankPath, typeof(GameObject));
-        
-        // Instantiate the prefabs in the scene
-        GameObject wall1 = Instantiate(wallPrefab) as GameObject;
-        GameObject woodPlank1 = Instantiate(woodPlankPrefab) as GameObject;
-        
-        // Position and rotate the objects
-        wall1.transform.position = new Vector3(-5, 0, -5);
-        wall1.transform.rotation = Quaternion.Euler(0, 90, 0);
-        woodPlank1.transform.position = new Vector3(-2, 0, -2);
-        woodPlank1.transform.rotation = Quaternion.Euler(0, 90, 0);
+        // Add fog effect to the scene
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = new Color(0.5f, 0.5f, 0.5f);
+        RenderSettings.fogDensity = 0.01f;
     }
 }
